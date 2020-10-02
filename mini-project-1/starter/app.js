1d3e972636ed39903de581b331afa3defa2318c7
jooprima <jooprima@gmail.com>
2020-10-02 21:03:18 +0700

    addTodo and DOM createElement

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -6,4 +6,29 @@ const filterInput = document.querySelector("#filter-input");
 const todoList = document.querySelector("#todo-list");
 const clearButton = document.querySelector("#clear-todos");
 
-console.log(todoForm)
+
+todoForm.addEventListener("submit",addTodo);
+
+function addTodo(e) {
+    e.preventDefault();
+    
+    //membuat Li element
+    const li = document.createElement("li")
+    li.className = "list-group-item d-flex justify-content-between align-items-center mb-1"
+
+    //menambahkan children ke dalam element li
+    li.appendChild(document.createTextNode("Value dari task input"))
+
+    //membuat delete button
+    const a = document.createElement("a");
+
+    //memberi properti untuk a element
+    a.href="#";
+    a.className="badge badge-danger"
+    a.innerHTML = "Delete"
+
+    //menyelipkan element a ke dalam element li
+    li.appendChild(a)
+
+    console.log(li);
+}
